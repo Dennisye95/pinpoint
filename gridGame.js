8c5e84880bc64ae0d7b06826b96d845bfbf01004
Dennis Ye <dennisye@Denniss-MacBook-Pro.local>
2018-06-12 09:46:05 -0400

    add dot

--- gridGame.js
+++ gridGame.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
   createGrid(5);
+  addDot(2,2,5);
+  addDot(4,3,5);
+  addDot(1,5,5);
 });
 
 function createGrid(size) {
@@ -8,7 +11,13 @@ function createGrid(size) {
     ", " + Math.floor(Math.random() * 256) + ")";
   console.log(color);
   for (var i=0; i<size*size; i++) {
-    $('.game').append('<div class="block"></div>')
+    $('.game').append('<div class="block"></div>');
     $('.game .block').css('background-color', color);
   }
+}
+
+function addDot(x, y, size) {
+  var index = (y - 1) * size + x;
+  $('.game .block').empty();
+  $('.game .block:nth-child(' + index + ')').html('<i class="dot fas fa-circle"></i>');
 }
